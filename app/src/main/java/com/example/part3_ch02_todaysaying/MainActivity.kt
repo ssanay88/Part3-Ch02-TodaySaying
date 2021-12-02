47d0c28d26d8eba4a13110cd51011f624e0b89cd
Yangsuhyuck <pplloo748@gmail.com>
2021-12-02 17:32:23 +0900

    2021-12-02

--- app/src/main/java/com/example/part3_ch02_todaysaying/MainActivity.kt
+++ app/src/main/java/com/example/part3_ch02_todaysaying/MainActivity.kt
@@ -3,12 +3,14 @@ package com.example.part3_ch02_todaysaying
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.util.Log
+import android.view.View
 import com.example.part3_ch02_todaysaying.databinding.ActivityMainBinding
 import com.google.firebase.ktx.Firebase
 import com.google.firebase.remoteconfig.ktx.remoteConfig
 import com.google.firebase.remoteconfig.ktx.remoteConfigSettings
 import org.json.JSONArray
 import org.json.JSONObject
+import kotlin.math.absoluteValue
 
 
 class MainActivity : AppCompatActivity() {
@@ -20,11 +22,35 @@ class MainActivity : AppCompatActivity() {
         mainBinding = ActivityMainBinding.inflate(layoutInflater)
         setContentView(mainBinding.root)
 
-
+        initViews()
         initData()
 
     }
 
+    private fun initViews() {
+        // setPageTransformer : 페이지를 스크롤 시 원하는 이펙트를 커스텀한다
+        // page : 변형을 적용할 페이지 타입 View
+        // position : 단순히 페이지 1,2,3..이 아닌 스크롤을 넘기는 만큼 변화하는 포지션을 나타낸다.
+        // 현재 보는 페이지를 0으로 두고 왼쪽으로 스크롤시 -0.5 , -1 ... 오른쪽은 이와 반대로 작용
+        mainBinding.viewPager.setPageTransformer { page, position ->
+            // 여기서는 투명도(alpha)만 컨트롤해준다.
+            when {
+                position.absoluteValue >= 1F -> {
+                    page.alpha = 0F
+                }
+
+                position == 0F -> {
+                    page.alpha = 1F
+                }
+
+                else -> {
+                    // alpha 값은 0~1사이 값이므로 가운데(0) 위치 시 alpha값을 1로 만들어줘야한다.
+                    page.alpha = 1F - 2 * position.absoluteValue
+                }
+            }
+
+        }
+    }
 
     // remoteConfig 원격 구성 로딩 전략 부분 -> 사용자가 사용하고 있을 때 UI를 변경시키는 행동을 피하자!
     // 앱을 처음 시작할 때 데이터를 로딩
@@ -41,6 +67,7 @@ class MainActivity : AppCompatActivity() {
         // 패치가 비동기적으로 진행되기 때문에 리스너 설정
         remoteConfig.fetchAndActivate().addOnCompleteListener {
             // 패치 작업이 완료될 경우
+            mainBinding.progressBar.visibility = View.GONE
             if (it.isSuccessful) {
                 val quotes = parseQuotesJson(remoteConfig.getString("quotes"))
                 val isNameRevealed = remoteConfig.getBoolean("is_name_revealed")
@@ -81,11 +108,16 @@ class MainActivity : AppCompatActivity() {
 
     private fun displayQuotesPager(quotes:List<Quote> , isNameRevealed:Boolean) {
 
-        mainBinding.viewPager.adapter = QuotesPagerAdapter(
+        val adapter = QuotesPagerAdapter(
             quotes = quotes,
-            isNameRevealed = isNameRevealed    // remote Config에서 조정 가능 true or false
-
+            isNameRevealed = isNameRevealed
         )
+
+
+        mainBinding.viewPager.adapter = adapter
+        // 아이템의 절반 위치로 이동시켜버린다. 두번째 인자는 이동 시 부드럽게 넘어가는 동작에 관한
+        // Boolean 값으로 여기서는 사용하지 않는다.
+        mainBinding.viewPager.setCurrentItem(adapter.itemCount/2, false)
 
     }
 
@@ -107,7 +139,7 @@ ViewPager2
 1. Firebase - Remote Config  !!정리따로 필요!!
 앱 업데이트를 게시하지 않아도 하루 활성 사용자 수 제한 없이 무료로 앱의 동작과 모양을 변경할 수 있도록 해준다.
 
-
+2. ViewPager2 : 뷰 페이지에 대한 여러가지 효과들을 작동시킨다.
  */
 
 
--- app/src/main/java/com/example/part3_ch02_todaysaying/QuotesPagerAdapter.kt
+++ app/src/main/java/com/example/part3_ch02_todaysaying/QuotesPagerAdapter.kt
@@ -1,5 +1,6 @@
 package com.example.part3_ch02_todaysaying
 
+import android.annotation.SuppressLint
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
@@ -15,24 +16,29 @@ class QuotesPagerAdapter(val quotes : List<Quote> , val isNameRevealed : Boolean
 
     override fun onBindViewHolder(holder: QuoteViewHolder, position: Int) {
 
-        holder.bind(quotes[position] , isNameRevealed )
+        // 무한한 슬라이딩을 위해 계속 반복시켜준다.
+        val actualPosition = position % quotes.size
+        holder.bind(quotes[actualPosition] , isNameRevealed )
 
     }
 
-    override fun getItemCount() = quotes.size
+    // 무한 슬라이드를 위해
+    override fun getItemCount() = Int.MAX_VALUE     // 최대한 큰값을 준다.
 
     class QuoteViewHolder(itemView: View) : RecyclerView.ViewHolder(itemView) {
 
         private val quoteTextView: TextView = itemView.findViewById(R.id.quoteTextView)
         private val nameTextView: TextView = itemView.findViewById(R.id.nameTextView)
 
-        fun bind(quote: Quote , isNameRevealed: Boolean) {
+        @SuppressLint("SetTextI18n")
+        fun bind(quote: Quote, isNameRevealed: Boolean) {
 
-            quoteTextView.text = quote.quote
+            quoteTextView.text = "\"${quote.quote}\""
+
 
             // 이름을 감추고 싶을 경우 True , 이름까지 표시할 경우 False
             if (isNameRevealed) {
-                nameTextView.text = quote.name
+                nameTextView.text =  "- ${quote.name}"
                 nameTextView.visibility = View.VISIBLE
             } else {
                 nameTextView.visibility = View.GONE
